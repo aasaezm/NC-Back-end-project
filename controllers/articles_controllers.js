fccda872de873daad7982c03338872fa87be76f8
quarki <62488270+aasaezm@users.noreply.github.com>
2022-02-16 13:18:42 +0000

    ncnews-7: patch articles complete

--- controllers/articles_controllers.js
+++ controllers/articles_controllers.js
@@ -1,9 +1,24 @@
-const { fetchArticleById } = require("../models/articles_models.js");
+const {
+  fetchArticleById,
+  updateArticleById,
+} = require("../models/articles_models.js");
 
 exports.getArticleById = (req, res, next) => {
   const { article_id } = req.params;
 
   fetchArticleById(article_id)
+    .then((article) => {
+      res.status(200).send(article);
+    })
+    .catch(next);
+};
+
+exports.patchArticleById = (req, res, next) => {
+  const {
+    body: { inc_votes },
+    params: { article_id },
+  } = req;
+  updateArticleById(article_id, inc_votes)
     .then((article) => {
       res.status(200).send(article);
     })
