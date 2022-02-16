8e5a50c426200ecf31e9cb059de81b9887806e91
quarki <62488270+aasaezm@users.noreply.github.com>
2022-02-16 15:48:45 +0000

    ncnews-21: get /api/users

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { getTopics } = require("./controllers/topics_controllers");
 const { getArticleById } = require("./controllers/articles_controllers");
+const { getUsers } = require("./controllers/users_controllers.js");
 const {
   handleCustomErrors,
   handlePsqlErrors,
@@ -17,8 +18,12 @@ app.get("/api/topics", getTopics);
 //Articles
 app.get("/api/articles/:article_id", getArticleById);
 
+//Users
+app.get("/api/users", getUsers);
+
 //Error handlers
 app.all("/*", (req, res) => {
+  console.log(res);
   res.status(404).send({ msg: "Path not found" });
 });
 app.use(handleCustomErrors);
