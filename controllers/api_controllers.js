82e3eb8d63f4ab1ba17de15550629beaf83ae323
quarki <62488270+aasaezm@users.noreply.github.com>
2022-03-01 18:10:36 +0000

    refactor api controller

--- controllers/api_controllers.js
+++ controllers/api_controllers.js
@@ -1,6 +1,6 @@
-const { fetchEndpoints } = require("../models/api_models.js");
+// const { fetchEndpoints } = require("../models/api_models.js");
 const endpoints = require("../endpoints.json");
 
 exports.getEndpoints = (req, res, next) => {
-  fetchEndpoints().then(res.status(200).json(endpoints)).catch(next);
+  res.status(200).json(endpoints).catch(next);
 };
