33a9316e40e56921e10d08047a4ce6c2695dc795
quarki <62488270+aasaezm@users.noreply.github.com>
2022-03-16 00:28:01 +0000

    Changed users models and tests. Now returns everything and not just the username

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -415,7 +415,7 @@ describe("app", () => {
     });
   });
 
-  describe("Users", () => {
+  describe.only("Users", () => {
     describe("GET - api/users", () => {
       test("Status 200 - Responds with an array of objects, and each object has a property of 'username'", () => {
         return request(app)
@@ -424,10 +424,30 @@ describe("app", () => {
           .then(({ body: { users } }) => {
             expect(Array.isArray(users)).toBe(true);
             expect(users).toEqual([
-              { username: "butter_bridge" },
-              { username: "icellusedkars" },
-              { username: "rogersop" },
-              { username: "lurker" },
+              {
+                username: "butter_bridge",
+                name: "jonny",
+                avatar_url:
+                  "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+              },
+              {
+                username: "icellusedkars",
+                name: "sam",
+                avatar_url:
+                  "https://avatars2.githubusercontent.com/u/24604688?s=460&v=4",
+              },
+              {
+                username: "rogersop",
+                name: "paul",
+                avatar_url:
+                  "https://avatars2.githubusercontent.com/u/24394918?s=400&v=4",
+              },
+              {
+                username: "lurker",
+                name: "do_nothing",
+                avatar_url:
+                  "https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png",
+              },
             ]);
           });
       });
